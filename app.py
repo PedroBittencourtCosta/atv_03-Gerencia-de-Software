a1e2e25d8a6ebeddff68755f912a44acdc74dcaf
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2025-08-30 22:00:44 -0300

    Feat: Implementa função de saudação

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-# Versão 1.1
+# Versão 1.2
+def saudacao(nome):
+    return f"Olá, {nome}! Bem-vindo(a)."
+
 autor = "Seu Nome"
-print(f"Software de Exemplo - v1.1 | Autor: {autor}")
+print(f"Software de Exemplo - v1.2 | Autor: {autor}")
+print(saudacao("Usuário"))
