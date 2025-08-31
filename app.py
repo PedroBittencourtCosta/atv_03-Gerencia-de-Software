78cf28f98ef1258ea9f6037b21ebe7ec4d0fd0bd
PedroBittencourtCosta <pedro.bittencourt.dev@gmail.com>
2025-08-30 22:02:48 -0300

    Feat: Adiciona um menu principal

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-# Versão 1.2
+# Versão 1.3 (em desenvolvimento)
 def saudacao(nome):
     return f"Olá, {nome}! Bem-vindo(a)."
 
+def mostrar_menu():
+    print("\n--- MENU ---")
+    print("1. Iniciar")
+    print("2. Sair")
+    print("------------")
+
 autor = "Seu Nome"
-print(f"Software de Exemplo - v1.2 | Autor: {autor}")
+print(f"Software de Exemplo | Autor: {autor}")
 print(saudacao("Usuário"))
+mostrar_menu()
